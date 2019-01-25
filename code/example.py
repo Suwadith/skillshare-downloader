54309d22da99aecc320bd9c904b159a1c314f3f8
Suwadith <suwadith.srithar@gmail.com>
2019-01-26 00:42:53 +0530

    apache-spark

--- code/example.py
+++ code/example.py
@@ -7,7 +7,7 @@ YII_CSRF_TOKEN=YmthQ3h4dTJmZFNZcTdrOEFFbEhQeU9XR0U1YWxUWEkIFy-SLi5-tAQpNf9yZlNcJ
 dl = Downloader(cookie=cookie)
 
 # download by class URL:
-dl.download_course_by_url('https://www.skillshare.com/classes/Building-Recommender-Systems-with-Machine-Learning-and-AI/1493261308')
+dl.download_course_by_url('https://www.skillshare.com/classes/Apache-Spark-2-with-Scala-Hands-On-with-Big-Data/95124690')
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
